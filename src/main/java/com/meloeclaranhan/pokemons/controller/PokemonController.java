dbe89799031eae34d41c41639948bc1991168bb6
henriqueclaranhan <henrique.claranhan@outlook.com>
2022-04-15 14:47:58 -0400

    adicionar rotas para editar e deletar pokémons

--- src/main/java/com/meloeclaranhan/pokemons/controller/PokemonController.java
+++ src/main/java/com/meloeclaranhan/pokemons/controller/PokemonController.java
@@ -1,6 +1,7 @@
 package com.meloeclaranhan.pokemons.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,35 +19,74 @@ import com.meloeclaranhan.pokemons.repository.PokemonRepository;
 
 
 @Controller
+@RequestMapping("/pokemon")
 public class PokemonController {
 	@Autowired
 	private PokemonRepository pokemonRepository;
-	
-	@RequestMapping("/")
-	public String homeRoute(Model model) {
-		return "index";
-	}
-	
-	@RequestMapping("/list")
-	public String listPokemonsRoute(Model model) {
-		List<Pokemon> pokes = pokemonRepository.findAll();
-		model.addAttribute("pokemons", pokes);
-		return "list-pokemons";
-	}
-	
+
 	@GetMapping("/add")
-	public String addPokemonRoute(Model model) {
-		Pokemon poke = new Pokemon();
-		model.addAttribute("pokemon", poke);
-		return "add-pokemon";
+	public String addPokemon(Model model) {
+		Pokemon pokemon = new Pokemon();
+
+		model.addAttribute("pokemon", pokemon);
+
+		return "public/add-pokemon";
 	}
-	
+
 	@PostMapping("/save")
-	public String savePokemonRoute(@Valid Pokemon poke, BindingResult result) {
-		if(result.hasErrors()) {
-			return "add-pokemon";
+	public String savePokemon(@Valid Pokemon pokemon, BindingResult result) {
+		if (result.hasErrors()) {
+			return "public/add-pokemon";
 		}
-		pokemonRepository.save(poke);
-		return "redirect:/list";
+
+		pokemonRepository.save(pokemon);
+
+		return "redirect:/pokemon/admin/list";
 	}
+
+	@RequestMapping("/admin/list")
+	public String listPokemons(Model model) {
+		List<Pokemon> pokemons = pokemonRepository.findAll();
+
+		model.addAttribute("pokemons", pokemons);
+
+		return "/auth/admin/list-pokemons";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String editPokemon(@PathVariable("id") long id, Model model) {
+		Optional<Pokemon> currentPokemon = pokemonRepository.findById(id);
+
+		if (!currentPokemon.isPresent()) {
+			throw new IllegalArgumentException("Id inválido: " + id);
+		}
+
+		Pokemon pokemon = currentPokemon.get();
+		model.addAttribute("pokemon", pokemon);
+
+		return "/auth/user/edit-pokemon";
+	}
+
+	@PostMapping("/edit/{id}")
+	public String editPokemon(@PathVariable("id") long id, @Valid Pokemon pokemon, BindingResult result) {
+		if (result.hasErrors()) {
+			pokemon.setId(id);
+			return "/auth/user/edit-pokemon";
+		}
+
+		pokemonRepository.save(pokemon);
+
+		return "redirect:/pokemon/admin/list";
+	}
+
+	@RequestMapping("/admin/delete/{id}")
+	public String deletePokemon(@PathVariable("id") long id, Model model) {
+		Pokemon pokemon = pokemonRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
+
+		pokemonRepository.delete(pokemon);		
+
+		return "redirect:/pokemon/admin/list";
+	}
+
 }
